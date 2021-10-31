57488ae7dd129040f298dca58c566146802b59b4
vlad-Kriajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2021-10-31 23:34:20 +0300

    Добавление Графиков Списков
    Добавление Графиков Списков

--- assets/js/Chart.js
+++ assets/js/Chart.js
@@ -1,5 +1,7 @@
-const ctx = document.getElementById('bubbleChart').getContext('2d');
-const bubbleChart = new Chart(ctx, {
+/* График Статистика Оперций*/
+
+const ctx = document.getElementById('operationStatisticsСhart').getContext('2d');
+const operationStatisticsСhart = new Chart(ctx, {
         type: 'bubble',
         data:  {
         labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', ],
@@ -29,17 +31,28 @@ const bubbleChart = new Chart(ctx, {
             legend: {
               display: false
             },
-            // title: {
-            //   display: true,
-            //   position: 'top',
-            //   text: 'Операции'
-            // }
+          },
+          scales:{
+            x:{
+              title: {
+                display: true,
+                text: 'Месяц'
+              },
+            },
+            y:{
+              title: {
+                display: true,
+                text: 'Кол-во Операций'
+              },
+            },
           }
         },
 });
 
-const ctx2 = document.getElementById('lineChart').getContext('2d');
-const lineChart = new Chart(ctx2, {
+
+/*  График  Прибыль от Комисии      */
+const ctx2 = document.getElementById('profitFromCommissionСhart').getContext('2d');
+const profitFromCommissionСhart = new Chart(ctx2, {
   
         type: 'line',
         data:  { labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', ],
@@ -65,11 +78,10 @@ const lineChart = new Chart(ctx2, {
             borderWidth: 1
         }],
     },
+
         options: {
+         
           responsive: true,
-          y:{
-            suggestedMax: 200,
-          },
           plugins: {
             legend: {
               display: false
@@ -81,6 +93,166 @@ const lineChart = new Chart(ctx2, {
           },
           scales: {
             y:{
+              suggestedMax: 200,
+              title: {
+                display: true,
+                text: 'Кол-во Операций'
+              },
+              grid:{
+              display: false,
+           },
+          },
+          x:{
+            title: {
+              display: true,
+              text: 'Месяц'
+            },
+            grid:{
+            display: false,
+          }
+        },
+        
+      },
+    }
+});
+
+/*  График  Статистика марженальности      */
+const ctx3 = document.getElementById('marginStatisticsChart').getContext('2d');
+const marginStatisticsChart = new Chart(ctx3, {
+  
+        type: 'line',
+        data:  { 
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
+        datasets: [{
+            label: 'Truck 1. Средний чек Операции',
+            data: [46, 21, 43, 30, 17, 99, 68, 55, 63, 20, 53, 29, 12, 22, 33, 4, 62, 81, 74, 58, 16, 67, 89, 71, 15, 35, 34, 56, 19, 73],
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            borderColor: [
+                'rgba(34, 43, 94, 1)',
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }],
+    },
+
+        options: {
+         
+          responsive: true,
+         
+          plugins: {
+            legend: {
+              display: false
+            },
+            title: {
+              display: false,
+              
+            },
+          },
+          scales: {
+            y:{
+              title: {
+                display: true,
+                text: 'Основной Показатель (Средний чек)'
+              },
+              suggestedMax: 200,
+              grid:{
+              display: false,
+           },
+          },
+          x:{
+            title: {
+              display: true,
+              text: 'Показатель Маржинальности'
+            },
+            grid:{
+            display: false,
+          }
+        },
+        
+      },
+    }
+});
+/* График Недельная Операция*/
+const ctx4 = document.getElementById('weeklyOperationChart').getContext('2d');
+const weeklyOperationChart = new Chart(ctx4, {
+  
+        type: 'bar',
+        data:  { labels: ['Понедельник','Вторник','Среда','Четверг','Пятница','Субота','Воскресенье ', ],
+        datasets: [{
+            label: 'truck 1. кол-во Оперций',
+            data: [46, 21, 43, 30, 17, 99, 68],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(34, 43, 94, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },{
+          label: 'Truc 3. кол-во Оперций',
+          data: [78, 11, 33, 50, 27, 79, 48],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(34, 43, 94, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }
+      
+      ],
+    },
+
+        options: {
+         
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false
+            },
+            title: {
+              display: false,
+              
+            },
+          },
+          scales: {
+            y:{
+              title:{
+                display: true,
+                text:'Кол-во Операций'
+              },
+              suggestedMax: 200,
               grid:{
               display: false,
            },
@@ -89,7 +261,80 @@ const lineChart = new Chart(ctx2, {
             grid:{
             display: false,
           }
-        }
+        },
+        
+      },
+    }
+});
+
+/*  График  Сравнение по периодам    */
+const ctx5 = document.getElementById('periodComparison').getContext('2d');
+const periodComparison = new Chart(ctx5, {
+  
+        type: 'line',
+        data:  { labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', ],
+        datasets: [{
+            label: 'of Votes',
+            data: [46, 21, 43, 30, 17, 99, 68, 55, 63, 20, 53, 29, 12, 22, 33, 4, 62, 81, 74, 58, 16, 67, 89, 71, 15, 35, 34, 56, 19, 73],
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            borderColor: [
+                'rgba(34, 43, 94, 1)',
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }],
+    },
+
+        options: {
+         
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false
+            },
+            title: {
+              display: false,
+              
+            },
+          },
+          scales: {
+            y:{
+              ticks: {
+                // Include a dollar sign in the ticks
+                callback: function(value, index, values) {
+                    return  value + ' $';
+                }
+              },
+              suggestedMax: 200,
+              title: {
+                display: true,
+                text: 'Кол-во Операций'
+              },
+              grid:{
+              display: false,
+           },
+          },
+          x:{
+            title: {
+              display: true,
+              text: 'Месяц'
+            },
+            grid:{
+            display: false,
+          }
+        },
+        
       },
     }
 });
---
+++ assets/js/showAllInfoWarning.js
@@ -0,0 +1,24 @@
+function showAllInfoWarning() {
+    var x = document.getElementById("showAllInfoWarning");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        x.style.transitionDuration = "1s";
+        
+    } else {
+        x.style.display = "block";
+        
+    }
+}
+
+function СonditionSystemShow() {
+    var x = document.getElementById("СonditionSystemShow");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        x.style.transitionDuration = "1s";
+        
+    } else {
+        x.style.display = "block";
+        
+    }
+}
+
