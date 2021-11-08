ea5299d2108d713c4613b4d966f32a9c505702ed
vlad-Kriajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2021-11-08 15:50:46 +0300

    Дороботки

---
+++ assets/js/clickOpen/clickOpenReinforcements.js
@@ -0,0 +1,6 @@
+document.getElementById('open').onclick = function() {
+    if(document.getElementById('AllInfoCar').classList.contains('active2')) {
+        document.getElementById('AllInfoCar').classList.remove('active2');
+    }else
+    document.getElementById('AllInfoCar').classList.add('active2');
+  }
